7a257aea3a97d7df69e90fb7eb1f59b9f6c38e0b
web-padawan <serguey.kulikov@gmail.com>
2019-02-12 21:57:25 +0200

    feat(white-label): add abw-checkbox

---
+++ packages/white-label/src/components/abw-checkbox.js
@@ -0,0 +1,18 @@
+import { CheckboxElement } from '@aybolit/core';
+import abwCheckboxStyles from './styles/abw-checkbox-css.js';
+
+export class AbwCheckbox extends CheckboxElement {
+  static get is() {
+    return 'abw-checkbox';
+  }
+
+  static get version() {
+    return '0.0.0';
+  }
+
+  static get styles() {
+    return [super.styles, abwCheckboxStyles];
+  }
+}
+
+customElements.define(AbwCheckbox.is, AbwCheckbox);
--- packages/white-label/src/index.js
+++ packages/white-label/src/index.js
@@ -1,2 +1,3 @@
+export { AbwCheckbox } from './components/abw-checkbox.js';
 export { AbwRange } from './components/abw-range.js';
 export { AbwSwitch } from './components/abw-switch.js';
