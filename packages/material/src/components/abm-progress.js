1176c24d151880294ddfbb871d6baf0ac9be151a
web-padawan <serguey.kulikov@gmail.com>
2019-02-24 17:28:39 +0200

    feat(material): add abm-progress

---
+++ packages/material/src/components/abm-progress.js
@@ -0,0 +1,10 @@
+import { ProgressElement } from '@aybolit/core';
+import abmProgressStyles from './styles/abm-progress-css.js';
+
+export class AbmProgress extends ProgressElement {
+  static get styles() {
+    return [super.styles, abmProgressStyles];
+  }
+}
+
+customElements.define('abm-progress', AbmProgress);
