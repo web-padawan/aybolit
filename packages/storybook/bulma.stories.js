080a9489c8f75cc0a47217e5be0e886acdb6c40e
web-padawan <serguey.kulikov@gmail.com>
2019-02-27 13:12:24 +0200

    refactor(bulma): use separate styling attributes

--- packages/storybook/bulma.stories.js
+++ packages/storybook/bulma.stories.js
@@ -22,31 +22,34 @@ const SIZES = {
 storiesOf('Bulma', module)
   .addDecorator(withKnobs)
   .add('<abu-button>', () => {
+    const label = text('Label', 'Button');
     const disabled = boolean('Disabled', false);
     const size = select('Size', SIZES, SIZES.normal);
-    const color = select('Color', COLORS, COLORS.none);
-    const label = text('Label', 'Button');
+    const theme = select('Theme', COLORS, COLORS.none);
     const link = text('Link', '');
     const outlined = boolean('Outlined', false);
     const rounded = boolean('Rounded', false);
     return html`
       <abu-button
-        theme="${size} ${color} ${outlined ? 'outlined' : ''} ${rounded ? 'rounded' : ''}"
         .disabled="${disabled}"
         .link="${link}"
+        theme="${theme}"
+        size="${size}"
+        ?outlined="${outlined}"
+        ?rounded="${rounded}"
       >
         ${label}
       </abu-button>
     `;
   })
   .add('<abu-checkbox>', () => {
+    const label = text('Label', 'Checkbox');
     const disabled = boolean('Disabled', false);
     const size = select('Size', SIZES, SIZES.normal);
-    const color = select('Color', COLORS, COLORS.none);
-    const label = text('Label', 'Checkbox');
+    const theme = select('Theme', COLORS, COLORS.none);
     const filled = boolean('Filled', false);
     return html`
-      <abu-checkbox theme="${size} ${color} ${filled ? 'filled' : ''}" .disabled="${disabled}">
+      <abu-checkbox theme="${theme}" size="${size}" ?filled="${filled}" .disabled="${disabled}">
         ${label}
       </abu-checkbox>
     `;
@@ -55,34 +58,39 @@ storiesOf('Bulma', module)
     const value = number('Value', 10);
     const max = number('Max', 100);
     const size = select('Size', SIZES, SIZES.normal);
-    const color = select('Color', COLORS, COLORS.none);
+    const theme = select('Theme', COLORS, COLORS.none);
     return html`
-      <abu-progress .value="${value}" .max="${max}" theme="${size} ${color}"></abu-progress>
+      <abu-progress .value="${value}" .max="${max}" theme="${theme}" size="${size}"></abu-progress>
     `;
   })
   .add('<abu-range>', () => {
     const disabled = boolean('Disabled', false);
     const size = select('Size', SIZES, SIZES.normal);
-    const color = select('Color', COLORS, COLORS.none);
-    const circle = boolean('Circle', false);
+    const theme = select('Theme', COLORS, COLORS.none);
+    const rounded = boolean('Rounded', false);
     return html`
       <abu-range
-        theme="${size} ${color} ${circle ? 'circle' : ''}"
+        theme="${theme}"
+        size="${size}"
+        ?rounded="${rounded}"
         .disabled="${disabled}"
       ></abu-range>
     `;
   })
   .add('<abu-switch>', () => {
+    const label = text('Label', 'Switch');
     const disabled = boolean('Disabled', false);
     const size = select('Size', SIZES, SIZES.normal);
-    const color = select('Color', COLORS, COLORS.none);
-    const label = text('Label', 'Switch');
+    const theme = select('Theme', COLORS, COLORS.none);
     const outlined = boolean('Outlined', false);
     const rounded = boolean('Rounded', false);
     return html`
       <abu-switch
-        theme="${size} ${color} ${outlined ? 'outlined' : ''} ${rounded ? 'rounded' : ''}"
         .disabled="${disabled}"
+        theme="${theme}"
+        size="${size}"
+        ?outlined="${outlined}"
+        ?rounded="${rounded}"
       >
         ${label}
       </abu-switch>
