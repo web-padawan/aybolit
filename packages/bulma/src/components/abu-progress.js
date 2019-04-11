6693baa49566549d2be1d64ee395ce5c4588d9da
Janar Palk <janar.palk@gmail.com>
2019-04-11 11:31:37 +0300

    chore: move styles folder from components to src (#21)

--- packages/bulma/src/components/abu-progress.js
+++ packages/bulma/src/components/abu-progress.js
@@ -1,6 +1,6 @@
 import { ProgressElement } from '@aybolit/core';
-import abuProgressStyles from './styles/abu-progress-css.js';
-import abuThemeStyles from './styles/abu-theme-css.js';
+import abuProgressStyles from '../styles/abu-progress-css.js';
+import abuThemeStyles from '../styles/abu-theme-css.js';
 
 export class AbuProgress extends ProgressElement {
   static get styles() {
--- packages/bulma/src/components/abu-range.js
+++ packages/bulma/src/components/abu-range.js
@@ -1,6 +1,6 @@
 import { RangeElement } from '@aybolit/core';
-import abuRangeStyles from './styles/abu-range-css.js';
-import abuThemeStyles from './styles/abu-theme-css.js';
+import abuRangeStyles from '../styles/abu-range-css.js';
+import abuThemeStyles from '../styles/abu-theme-css.js';
 
 export class AbuRange extends RangeElement {
   static get styles() {
