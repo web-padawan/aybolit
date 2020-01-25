8603943192279f1ff2a996d9d4c1a0257706d686
Leho Kraav <leho@kraav.com>
2020-01-25 16:51:00 +0200

    refactor(storybook): cxl-accordion-card hyperlink title, featured image +peep

--- packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-accordion-card.stories.js
+++ packages/storybook/cxl-ui/cxl-vaadin-accordion/cxl-accordion-card.stories.js
@@ -55,17 +55,23 @@ export const CxlVaadinAccordionThemeArchive = () => {
               : ''}"
           >
             <header class="entry-header" slot="summary">
-              <h3 class="entry-title no-anchor" itemprop="headline">${el.title.raw}</h3>
-              <img
-                class="landscape cw-greater thumbnail shop_catalog lazyloaded"
-                alt="${el.title.raw}"
-                itemprop="image"
-                src="${el.cxl_featured_media.shop_catalog}"
-                data-src="${el.cxl_featured_media.shop_catalog}"
-                loading="lazy"
-                width="150"
-                height="150"
-              />
+              <h3 class="entry-title no-anchor" itemprop="headline">
+                <a href="${el.conversionxl_certificate_sales_page}" rel="bookmark" itemprop="url"
+                  >${el.title.raw}</a
+                >
+              </h3>
+              <a href="${el.conversionxl_certificate_sales_page}" rel="bookmark" itemprop="url">
+                <img
+                  class="landscape cw-greater thumbnail shop_catalog lazyloaded"
+                  alt="${el.title.raw}"
+                  itemprop="image"
+                  src="${el.cxl_featured_media.shop_catalog}"
+                  data-src="${el.cxl_featured_media.shop_catalog}"
+                  loading="lazy"
+                  width="150"
+                  height="150"
+                />
+              </a>
               <div class="entry-byline">
                 <iron-icon icon="lumo:clock"></iron-icon>${el.conversionxl_live_course_duration}
                 <hr />
