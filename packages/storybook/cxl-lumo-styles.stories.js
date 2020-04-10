bfbc5af03ae78e385510a1e1289c9ffcf01381e7
Leho Kraav <leho@kraav.com>
2020-04-10 13:18:03 +0300

    refactor(storybook): cxl-lumo-styles more `<vaadin-button>` examples

--- packages/storybook/cxl-lumo-styles.stories.js
+++ packages/storybook/cxl-lumo-styles.stories.js
@@ -36,9 +36,20 @@ export const VaadinButton = () => {
   const label = text('Label', 'Button');
 
   return html`
-    <vaadin-button>
-      ${label}
-    </vaadin-button>
+    <h6>Basic</h6>
+    <vaadin-button>${label}</vaadin-button>
+    <h6>Disabled</h6>
+    <vaadin-button disabled>${label}</vaadin-button>
+    <h6>Primary</h6>
+    <vaadin-button theme="primary">${label}</vaadin-button>
+    <h6>Secondary</h6>
+    <vaadin-button>${label}</vaadin-button>
+    <h6>Tertiary</h6>
+    <vaadin-button theme="tertiary">${label}</vaadin-button>
+    <h6>Tertiary inline</h6>
+    <vaadin-button theme="tertiary-inline">${label}</vaadin-button>
+    <h6>Upstream</h6>
+    <p>Also see <a href="https://vaadin.com/components/vaadin-button/html-examples">https://vaadin.com/components/vaadin-button/html-examples</a></p>
   `;
 };
 
