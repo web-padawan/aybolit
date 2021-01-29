0bc57ec539bf2a33894d2adc284845d49d4b24de
Leho Kraav <leho@kraav.com>
2021-01-29 10:30:51 +0200

    fix(storybook): unbreak web components vs HMR, fixup 2b7137a, close #82

--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -1,3 +1,19 @@
+import { configure } from '@storybook/web-components';
+
 export const parameters = {
   layout: 'none' // disable default 'padded'
 };
+
+// Needed to force a full page reload as custom element definitions tend to conflict
+// @see https://github.com/storybookjs/storybook/blob/next/examples/web-components-kitchen-sink/.storybook/preview.js#L29
+const req = require.context('../', true, /\.stories\.(js|mdx)$/);
+
+configure(req, module);
+
+if (module.hot) {
+  module.hot.accept(req.id, () => {
+    const currentLocationHref = window.location.href;
+    window.history.pushState(null, null, currentLocationHref);
+    window.location.reload();
+  });
+}
