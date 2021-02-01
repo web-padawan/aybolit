a26001d86927bbbb8dbc3145f0293d38f6d53459
Leho Kraav <leho@kraav.com>
2021-02-01 15:30:03 +0200

    fix(storybook): avoid double `configure`, fixup 0bc57ec [1]
    1: https://github.com/storybookjs/storybook/issues/12307

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
-  stories: ['../**/*.stories.js'],
-  addons: ['@storybook/addon-knobs', '@storybook/addon-viewport']
+  // @see https://github.com/storybookjs/storybook/issues/12307
+  // stories: ['../**/*.stories.js'],
+  addons: ['@storybook/addon-knobs', '@storybook/addon-viewport'],
 };
