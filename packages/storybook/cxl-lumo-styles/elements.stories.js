bde6e2260beac75dd058466555e04a2739b079b7
saasfreelancer <syed.sayal.shah@gmail.com>
2021-03-01 19:24:31 +0500

    refactor(storybook): vaadin-notification

--- packages/storybook/cxl-lumo-styles/elements.stories.js
+++ packages/storybook/cxl-lumo-styles/elements.stories.js
@@ -37,3 +37,45 @@ export const VaadinButton = () => {
 };
 
 VaadinButton.storyName = '<vaadin-button>';
+
+export const VaadinNotification = () => {
+  const message = text('Notification.', 'My message');
+
+  return html`
+    <vaadin-notification opened position="top-stretch" duration="-1" theme="success">
+      <template><div>Position: top-stretch ${message}</div></template>
+    </vaadin-notification>
+    <vaadin-notification opened position="top-start" duration="-1" theme="success">
+      <template><div>Position: top-start ${message}</div></template>
+    </vaadin-notification>
+    <vaadin-notification opened position="top-center" duration="-1" theme="success">
+      <template>Position: top-center ${message}</template>
+    </vaadin-notification>
+    <vaadin-notification opened position="top-end" duration="-1" theme="success">
+      <template>Position: top-end ${message}</template>
+    </vaadin-notification>
+    <vaadin-notification opened position="middle" duration="-1" theme="info">
+      <template>Position: middle ${message}</template>
+    </vaadin-notification>
+    <vaadin-notification opened position="bottom-start" duration="-1" theme="info">
+      <template>Position: bottom-start ${message}</template>
+    </vaadin-notification>
+    <vaadin-notification opened position="bottom-center" duration="-1" theme="info">
+      <template>Position: bottom-center ${message}</template>
+    </vaadin-notification>
+    <vaadin-notification opened position="bottom-end" duration="-1" theme="info">
+      <template>Position: bottom-end ${message}</template>
+    </vaadin-notification>
+    <vaadin-notification opened position="bottom-stretch" duration="-1" theme="error">
+      <template>Position: bottom-stretch ${message}</template>
+    </vaadin-notification>
+    <p>
+      Also see
+      <a href="https://vaadin.com/components/vaadin-notification/html-examples"
+        >https://vaadin.com/components/vaadin-notification/html-examples</a
+      >
+    </p>
+  `;
+};
+
+VaadinNotification.storyName = '<vaadin-notification>';
