e3e61bec34cb040d6ac0ada65aedbca470cf2333
leho <leho@conversionready.com>
2021-03-02 21:34:52 +0200

    feat(cxl-ui): add vaadin-notification

--- packages/cxl-lumo-styles/src/themes.js
+++ packages/cxl-lumo-styles/src/themes.js
@@ -4,6 +4,8 @@ import cxlVaadinAccordionStyles from './styles/themes/vaadin-accordion-css.js';
 import cxlVaadinAccordionPanelStyles from './styles/themes/vaadin-accordion-panel-css.js';
 import cxlVaadinContextMenuItemStyles from './styles/themes/vaadin-context-menu-item-css.js';
 import cxlVaadinContextMenuListBoxStyles from './styles/themes/vaadin-context-menu-list-box-css.js';
+import cxlVaadinNotificationCardStyles from './styles/themes/vaadin-notification-card-css.js';
+import cxlVaadinNotificationContainerStyles from './styles/themes/vaadin-notification-container-css.js';
 import cxlVaadinOverlayStyles from './styles/themes/vaadin-overlay-css.js';
 import cxlVaadinTabStyles from './styles/themes/vaadin-tab-css.js';
 import cxlVaadinTabsStyles from './styles/themes/vaadin-tabs-css.js';
@@ -23,6 +25,12 @@ registerStyles('vaadin-context-menu-item', cxlVaadinContextMenuItemStyles, {
 });
 registerStyles('vaadin-context-menu-list-box', cxlVaadinContextMenuListBoxStyles, {
   moduleId: 'cxl-vaadin-context-menu-list-box-styles',
+});
+registerStyles('vaadin-notification-card', cxlVaadinNotificationCardStyles, {
+  moduleId: 'cxl-vaadin-notification-card-styles',
+});
+registerStyles('vaadin-notification-container', cxlVaadinNotificationContainerStyles, {
+  moduleId: 'cxl-vaadin-notification-container-styles',
 });
 registerStyles('vaadin-overlay', cxlVaadinOverlayStyles, {
   moduleId: 'cxl-vaadin-overlay-styles',
