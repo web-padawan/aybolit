d879daef30b6c1710cfb05f6a052ae63636e3d01
Leho Kraav <leho@kraav.com>
2021-03-03 15:46:58 +0200

    refactor(storybook): vaadin-notification story upgrade

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
   // @see https://github.com/storybookjs/storybook/issues/12307
   // stories: ['../**/*.stories.js'],
-  addons: ['@storybook/addon-knobs', '@storybook/addon-viewport'],
+  addons: ['@storybook/addon-controls', '@storybook/addon-knobs', '@storybook/addon-viewport'],
 };
--- packages/storybook/cxl-lumo-styles/elements.stories.js
+++ packages/storybook/cxl-lumo-styles/elements.stories.js
@@ -1,81 +1,95 @@
 import '@conversionxl/cxl-lumo-styles';
 import '@vaadin/vaadin-button';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import '@vaadin/vaadin-notification';
 import { html } from 'lit-html';
 
 export default {
-  decorators: [withKnobs],
   title: 'CXL Lumo Styles/Elements',
 };
 
-export const VaadinButton = () => {
-  const label = text('Label', 'Button');
+export const VaadinButton = ({ Label }) => html`
+  <h6>Basic</h6>
+  <vaadin-button>${Label}</vaadin-button>
+  <h6>Disabled</h6>
+  <vaadin-button disabled>${Label}</vaadin-button>
+  <h6>Primary</h6>
+  <vaadin-button theme="primary">${Label}</vaadin-button>
+  <h6>Primary Contrast</h6>
+  <vaadin-button theme="primary contrast">${Label}</vaadin-button>
+  <h6>Secondary</h6>
+  <vaadin-button>${Label}</vaadin-button>
+  <h6>Tertiary</h6>
+  <vaadin-button theme="tertiary">${Label}</vaadin-button>
+  <h6>Tertiary inline</h6>
+  <vaadin-button theme="tertiary-inline">${Label}</vaadin-button>
+  <h6>Upstream</h6>
+  <p>
+    Also see
+    <a href="https://vaadin.com/components/vaadin-button/html-examples"
+      >https://vaadin.com/components/vaadin-button/html-examples</a
+    >
+  </p>
+`;
 
-  return html`
-    <h6>Basic</h6>
-    <vaadin-button>${label}</vaadin-button>
-    <h6>Disabled</h6>
-    <vaadin-button disabled>${label}</vaadin-button>
-    <h6>Primary</h6>
-    <vaadin-button theme="primary">${label}</vaadin-button>
-    <h6>Primary Contrast</h6>
-    <vaadin-button theme="primary contrast">${label}</vaadin-button>
-    <h6>Secondary</h6>
-    <vaadin-button>${label}</vaadin-button>
-    <h6>Tertiary</h6>
-    <vaadin-button theme="tertiary">${label}</vaadin-button>
-    <h6>Tertiary inline</h6>
-    <vaadin-button theme="tertiary-inline">${label}</vaadin-button>
-    <h6>Upstream</h6>
-    <p>
-      Also see
-      <a href="https://vaadin.com/components/vaadin-button/html-examples"
-        >https://vaadin.com/components/vaadin-button/html-examples</a
-      >
-    </p>
-  `;
-};
+Object.assign(VaadinButton, {
+  args: {
+    Label: 'Button',
+  },
+  storyName: '<vaadin-button>',
+});
 
-VaadinButton.storyName = '<vaadin-button>';
+export const VaadinNotification = ({ Duration, Position, Theme }) => html`
+  <vaadin-notification position="${Position}" duration="${Duration}" theme="${Theme}">
+    <template>
+      <!-- Must be static, or story will crash when editing Message control. -->
+      <div>Eat, sleep and pray well.</div>
+    </template>
+  </vaadin-notification>
+  <vaadin-button theme="primary" onclick="document.querySelector('vaadin-notification').open()"
+    >Trigger notification</vaadin-button
+  >
+  <p>
+    We have optimized upstream flexbox CSS specifically for
+    <strong>"(bottom|top)-center"</strong> and <strong>"middle"</strong> positions.
+  </p>
+  <p>This causes display glitches for other positions.</p>
+  <p>
+    Also see
+    <a href="https://vaadin.com/components/vaadin-notification/html-examples"
+      >https://vaadin.com/components/vaadin-notification/html-examples</a
+    >
+  </p>
+`;
 
-export const VaadinNotification = () => {
-  const message = text('Notification.', 'My message');
-
-  return html`
-    <vaadin-notification opened position="top-stretch" duration="-1" theme="success">
-      <template><div>Position: top-stretch ${message}</div></template>
-    </vaadin-notification>
-    <vaadin-notification opened position="top-start" duration="-1" theme="success">
-      <template><div>Position: top-start ${message}</div></template>
-    </vaadin-notification>
-    <vaadin-notification opened position="top-center" duration="-1" theme="success">
-      <template>Position: top-center ${message}</template>
-    </vaadin-notification>
-    <vaadin-notification opened position="top-end" duration="-1" theme="success">
-      <template>Position: top-end ${message}</template>
-    </vaadin-notification>
-    <vaadin-notification opened position="middle" duration="-1" theme="info">
-      <template>Position: middle ${message}</template>
-    </vaadin-notification>
-    <vaadin-notification opened position="bottom-start" duration="-1" theme="info">
-      <template>Position: bottom-start ${message}</template>
-    </vaadin-notification>
-    <vaadin-notification opened position="bottom-center" duration="-1" theme="info">
-      <template>Position: bottom-center ${message}</template>
-    </vaadin-notification>
-    <vaadin-notification opened position="bottom-end" duration="-1" theme="info">
-      <template>Position: bottom-end ${message}</template>
-    </vaadin-notification>
-    <vaadin-notification opened position="bottom-stretch" duration="-1" theme="error">
-      <template>Position: bottom-stretch ${message}</template>
-    </vaadin-notification>
-    <p>
-      Also see
-      <a href="https://vaadin.com/components/vaadin-notification/html-examples"
-        >https://vaadin.com/components/vaadin-notification/html-examples</a
-      >
-    </p>
-  `;
-};
-
-VaadinNotification.storyName = '<vaadin-notification>';
+Object.assign(VaadinNotification, {
+  args: {
+    Duration: 3000,
+    Position: 'top-center',
+    Theme: 'default',
+  },
+  argTypes: {
+    Position: {
+      control: {
+        options: [
+          'top-stretch',
+          'top-start',
+          'top-center',
+          'top-end',
+          'middle',
+          'bottom-start',
+          'bottom-center',
+          'bottom-end',
+          'bottom-stretch',
+        ],
+        type: 'inline-radio',
+      },
+    },
+    Theme: {
+      control: {
+        options: ['default', 'primary', 'contrast', 'success', 'error'],
+        type: 'inline-radio',
+      },
+    },
+  },
+  storyName: '<vaadin-notification>',
+});
