32a3a20bb9595a166bd8eb8ad757c50d6567fb52
Andrew Noblet <andrewbnoblet@gmail.com>
2021-04-27 12:51:22 -0400

    refactor(vaadin-tabs): update stories

--- packages/storybook/cxl-lumo-styles/vaadin-tabs/vaadin-tabs[theme=cxl-tabs-slider].stories.js
+++ packages/storybook/cxl-lumo-styles/vaadin-tabs/vaadin-tabs[theme=cxl-tabs-slider].stories.js
@@ -1,47 +1,82 @@
 import { html } from 'lit-html';
+import '../../../cxl-ui/src/components/cxl-card';
 
 export default {
   title: 'CXL Lumo Styles/Elements/vaadin-tabs',
 };
 
 export const Default = () => html`
+  <style>
+    html {
+      font-family: var(--lumo-font-family);
+    }
+
+    .entry {
+      transition: all 0.2s ease-in;
+    }
+
+    img {
+      display: block;
+    }
+
+    .testimonials-container {
+      font-family: var(--lumo-font-family);
+      display: flex;
+      flex-flow: row;
+      overflow-x: scroll;
+      max-width: 90%;
+      margin: 0 auto;
+    }
+
+    p,
+    blockquote {
+      margin-top: 0.5em;
+      margin-bottom: 0.75em;
+    }
+
+    vaadin-tab[selected] {
+      color: inherit;
+    }
+  </style>
+
   <vaadin-tabs orientation="horizontal" theme="cxl-tabs-slider minimal">
-    <vaadin-tab>
-      <figure>
-        <img
-          src="http://jobs.uprecords.org/wp-content/uploads/sites/4/2020/01/slide-img-1.jpg"
-          alt=""
-          data-id="220"
-          data-full-url="http://jobs.uprecords.org/wp-content/uploads/sites/4/2020/01/slide-img-1.jpg"
-          data-link="http://jobs.uprecords.org/sample-page/slide-img-1-2/"
-          class="wp-image-220"
-        />
-      </figure>
-    </vaadin-tab>
-    <vaadin-tab>
-      <figure>
-        <img
-          src="http://jobs.uprecords.org/wp-content/uploads/sites/4/2020/01/slide-img-2.jpg"
-          alt=""
-          data-id="221"
-          data-full-url="http://jobs.uprecords.org/wp-content/uploads/sites/4/2020/01/slide-img-2.jpg"
-          data-link="http://jobs.uprecords.org/sample-page/slide-img-2/"
-          class="wp-image-221"
-        />
-      </figure>
-    </vaadin-tab>
-    <vaadin-tab>
-      <figure>
-        <img
-          src="http://jobs.uprecords.org/wp-content/uploads/sites/4/2020/01/slide-img-3.jpg"
-          alt=""
-          data-id="222"
-          data-full-url="http://jobs.uprecords.org/wp-content/uploads/sites/4/2020/01/slide-img-3.jpg"
-          data-link="http://jobs.uprecords.org/sample-page/slide-img-3-2/"
-          class="wp-image-222"
-        />
-      </figure>
-    </vaadin-tab>
+    ${Array(7)
+      .fill(
+        html`
+          <vaadin-tab>
+            <cxl-card theme="cxl-testimonial">
+              <article class="entry">
+                <header class="card-entry-header">
+                  <img
+                    class="card-headshot"
+                    src="https://cxl.com/institute/wp-content/uploads/2020/05/48192546_10156982340630746_8127333122065825792_n-wpv_400pxx400px_center_center.jpg"
+                  />
+                  <img
+                    class="card-company-logo"
+                    src="https://cxl.com/institute/wp-content/uploads/2020/05/Screenshot-2020-05-27-at-13.28.50.png"
+                  />
+                  <h4>Kurt S.</h4>
+                  <p>Web Conversion Manager @ Pipedrive</p>
+                  <p>Estonia</p>
+                </header>
+
+                <div class="card-entry-summary">
+                  <p>
+                    Having completed the Conversion Optimization mini degree, this has given me a
+                    perfect overall base for my current role in marketing where I’m trying to always
+                    improve the web experience.
+                  </p>
+                  <p>
+                    CXL Institute is constantly putting out new material and also updating existing
+                    material to be up to date with the current best practises.
+                  </p>
+                </div>
+              </article>
+            </cxl-card>
+          </vaadin-tab>
+        `
+      )
+      .map((item) => item)}
   </vaadin-tabs>
 `;
 
--- packages/storybook/cxl-ui/cxl-card/theme=cxl-testimonial.stories.js
+++ packages/storybook/cxl-ui/cxl-card/theme=cxl-testimonial.stories.js
@@ -20,15 +20,6 @@ export const CXLTestimonial = () => html`
       display: block;
     }
 
-    .testimonials-container {
-      font-family: var(--lumo-font-family);
-      display: flex;
-      flex-flow: row;
-      overflow-x: scroll;
-      max-width: 90%;
-      margin: 0 auto;
-    }
-
     p,
     blockquote {
       margin-top: 0.5em;
@@ -36,43 +27,35 @@ export const CXLTestimonial = () => html`
     }
   </style>
 
-  <div class="testimonials-container">
-    ${Array(7)
-      .fill(
-        html`
-          <cxl-card theme="cxl-testimonial">
-            <article class="entry">
-              <header class="card-entry-header">
-                <img
-                  class="card-headshot"
-                  src="https://cxl.com/institute/wp-content/uploads/2020/05/48192546_10156982340630746_8127333122065825792_n-wpv_400pxx400px_center_center.jpg"
-                />
-                <img
-                  class="card-company-logo"
-                  src="https://cxl.com/institute/wp-content/uploads/2020/05/Screenshot-2020-05-27-at-13.28.50.png"
-                />
-                <h4>Kurt S.</h4>
-                <p>Web Conversion Manager @ Pipedrive</p>
-                <p>Estonia</p>
-              </header>
+  <cxl-card theme="cxl-testimonial">
+    <article class="entry">
+      <header class="card-entry-header">
+        <img
+          class="card-headshot"
+          src="https://cxl.com/institute/wp-content/uploads/2020/05/48192546_10156982340630746_8127333122065825792_n-wpv_400pxx400px_center_center.jpg"
+        />
+        <img
+          class="card-company-logo"
+          src="https://cxl.com/institute/wp-content/uploads/2020/05/Screenshot-2020-05-27-at-13.28.50.png"
+        />
+        <h4>Kurt S.</h4>
+        <p>Web Conversion Manager @ Pipedrive</p>
+        <p>Estonia</p>
+      </header>
 
-              <div class="card-entry-summary">
-                <p>
-                  Having completed the Conversion Optimization mini degree, this has given me a
-                  perfect overall base for my current role in marketing where I’m trying to always
-                  improve the web experience.
-                </p>
-                <p>
-                  CXL Institute is constantly putting out new material and also updating existing
-                  material to be up to date with the current best practises.
-                </p>
-              </div>
-            </article>
-          </cxl-card>
-        `
-      )
-      .map((item) => item)}
-  </div>
+      <div class="card-entry-summary">
+        <p>
+          Having completed the Conversion Optimization mini degree, this has given me a perfect
+          overall base for my current role in marketing where I’m trying to always improve the web
+          experience.
+        </p>
+        <p>
+          CXL Institute is constantly putting out new material and also updating existing material
+          to be up to date with the current best practises.
+        </p>
+      </div>
+    </article>
+  </cxl-card>
 `;
 
 CXLTestimonial.story = {
